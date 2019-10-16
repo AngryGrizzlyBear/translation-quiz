78756b667b9273e999d037c1032556342867cd19
Evan <evanmlongwood@gmail.com>
2019-10-16 16:49:57 -0700

    Uploading for remote work.

---
+++ src/Components/InputHolder.tsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Words extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      error: null,
+    };
+  }
+  render() {
+    return <p>Okay</p>;
+  }
+}

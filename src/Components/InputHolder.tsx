a5580ba5560568097a6d3d70a2a54c35f50329de
Evan <evanmlongwood@gmail.com>
2019-10-17 15:40:13 -0700

    Added Entry Box

--- src/Components/InputHolder.tsx
+++ src/Components/InputHolder.tsx
@@ -1,14 +1,50 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 
-class Words extends React.Component {
-  constructor(props) {
+interface WordsState {
+  items: Array<string>;
+  error?: string;
+  value: string;
+}
+
+interface Props {
+  something?: string;
+}
+
+class Words extends Component<Props, WordsState> {
+  constructor(props : Props) {
     super(props);
     this.state = {
       items: [],
-      error: null,
+      value: '',
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChange(event : any) {
+    this.setState({value: event.target.value});
+  }
+  handleSubmit(event : any ) {
+    alert(`A word was submitted${this.state.value}`)
+    event.preventDefault();
+  }
+
   render() {
-    return <p>Okay</p>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
   }
 }
+
+export default Words
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import NameLabel from './NameLabel'
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import NameLabel from './NameLabel';
+import Words from './Components/InputHolder';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NameLabel name={"Evanescese"} />
+        <NameLabel name={'Evanescese'} />
+        <Words />
       </div>
     );
   }
